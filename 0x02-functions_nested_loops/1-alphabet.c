eb6d56f2623d4cb6f3baf6e570f2be6e6272fb07
ciscoliving <fdegarson@gmail.com>
2022-07-05 14:29:03 -0700

    loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,10 @@
  */
 void print_alphabet(void)
 {
-	char alphabet;
+	char letter;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		_putchar(alphabet);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-	_putchar('\n')
+	_putchar('\n');
 }
