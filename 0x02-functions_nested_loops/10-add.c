1f6a7f5f429201147b661dbeb5fa8567cb13d6ce
ciscoliving <fdegarson@gmail.com>
2022-07-06 04:44:17 -0700

    loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * add - adds two lines of integers.
+ * @num1: represents the first integer
+ * @num2: represents the second integer
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
