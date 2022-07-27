7de598e331a33add87b2239cc9bfb342727a2cf6
Cisco <106821245+ciscoliving@users.noreply.github.com>
2022-07-28 00:26:02 +0300

    changed lib

---
+++ 0x09-static_libraries/int_abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @a: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _abs(int a)
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/int_isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isalpha(int c)
+{
+	return (0);
+}
