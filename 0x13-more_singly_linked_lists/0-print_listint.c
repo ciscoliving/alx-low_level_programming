b954288beaee0f06daadde742563142a82b8fc2e
ciscoliving <fdegarson@gmail.com>
2022-09-06 09:32:59 -0700

    linked

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all elements of listint_t
+ * @h: points to start of list
+ * 
+ * Return: size_t (number of nodes)Return: size_t (number of nodes)
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *tmp;
+	int i = 0;
+
+	tmp = h;
+	while (tmp != NULL)
+	{
+		if (tmp->n)
+		{
+			printf("%d\n", tmp->n);
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
